2ea53bdf20c814250e79e9d0584b12770f156a15
samuel powers <powerss7@mail.uc.edu>
2022-09-22 13:32:52 -0400

    it works

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("what is your party affiliation Democrat, Republican, Independent, or other ");
+        String PartyAffiliation = "O";
+
+        if(PartyAffiliation.equalsIgnoreCase("R"))
+        {
+            System.out.println("you are a Republican Elephant");
+        }
+        else if(PartyAffiliation.equalsIgnoreCase("D"))
+        {
+            System.out.println("you are a Democrat Donkey");
+        }
+        else if(PartyAffiliation.equalsIgnoreCase("I"))
+        {
+            System.out.println("you are a Independent Person");
+        }
+        else
+        {
+            System.out.println("you are another party");
+        }
     }
 }
